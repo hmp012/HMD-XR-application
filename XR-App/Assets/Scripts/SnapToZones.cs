c57df989156c9ec7c9a56cb7d21048a534a7c359
hmp012 <331305@via.dk>
2025-11-16 15:41:04 +0100

    donuts will attatch to base or to another donut (#7)

---
+++ XR-App/Assets/Scripts/SnapToZones.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapToZones : MonoBehaviour
+{
+    private List<SnapZone> _snapZones = new List<SnapZone>();
+    public float snapRadius = 0.5f;
+    private Vector3 _originalPosition;
+
+    void Start()
+    {
+        _snapZones = new(FindObjectsByType<SnapZone>(FindObjectsSortMode.None));
+    }
+
+    public void OnGrab()
+    {
+        _originalPosition = transform.position;
+    }
+
+
+    public void OnRelease()
+    {
+        Transform nearest = null;
+        float minDist = Mathf.Infinity;
+
+        foreach (var zone in _snapZones)
+        {
+            float dist = Vector3.Distance(transform.position, zone.transform.position);
+            if (dist < minDist)
+            {
+                minDist = dist;
+                nearest = zone.transform;
+            }
+        }
+
+        if (nearest != null && minDist <= snapRadius)
+        {
+            transform.position = new Vector3(_originalPosition.x, nearest.position.y + 0.1f, nearest.position.z);
+        }
+        else
+        {
+            transform.position = _originalPosition;
+        }
+    }
+}
